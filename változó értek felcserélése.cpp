1b4ea88525d171756ff031ce1f492bf4aa53f770
ttdoni99 <43473991+ttdoni99@users.noreply.github.com>
2019-02-27 20:40:37 +0100

    Create változó értek felcserélése.cpp

---
+++ változó értek felcserélése.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+
+int a= 2;
+int b= 5;
+cout<<"Eredeti számok értéke:"<<a<< "és"<<b<<endl;
+a= a+b;
+b=a-b;
+a=a-b;
+cout<<"A felcserélt értékek:"<<a<<"és" << b<<endl;
+
+
+
+}
